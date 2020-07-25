7d7aeb914e7cbc69b655c631ab0a7ed91d23c68b
zmante <mante.zelzyte-bernaciak.18@ucl.ac.uk>
2020-07-25 18:59:02 +0100

    Diagnostics and analytics

---
+++ tvpVAR/analytics.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tvpVAR.utils.ir_vecm_sv import ir_vecm_sv
+from tvpVAR.utils.ir_plots import ir_plots
+
+
+# Load the relevant np.ndarrays from MCMC sampler results file saved in .npz format
+data = np.load('resultsMCMC.npz')
+s_beta = data['s_beta']
+s_Sig = data['s_Sig']
+cidx = data['cidx']
+bidx = data['bidx']
+s_om = data['s_om']
+svsims = data['svsims'].item()
+dscale = data['dscale'].reshape((-1, 1))
+dscale_surp = data['dscale_surp'].reshape((-1, 1))
+p = data['p'].item()
+
+# Plot the time invariant/constant param probabilities
+p_tiv0 = np.sum(s_om == 0, axis=1)/ svsims
+p_tiv = np.zeros(p_tiv0.shape)
+p_tiv[:np.count_nonzero(cidx)] = p_tiv0[cidx.ravel()]
+p_tiv[np.count_nonzero(cidx):] = p_tiv0[bidx.ravel()]
+# Plotting
+plt.plot(p_tiv, marker='x', c='teal', linestyle='dashed', linewidth=1)
+plt.ylabel('TIV probability')
+plt.xlabel('Parameter Index')
+plt.title('Time Invariance Probability by Parameter')
+plt.show()
+
+# Impulse responses
+scale_adj = dscale * (1 / dscale.T)
+per = np.arange(1959.5, 2011.75, 0.25)
+t_start = np.argwhere(per == 2000).ravel()
+s = 20
+ab2, c, svars, ir, err = ir_vecm_sv(s_beta, s_Sig, cidx, t_start, s, 2.08 * dscale[2] / dscale[0], p,
+                                    np.diag((1 / dscale_surp).ravel()) @ np.array([[1, -1, 0], [0, 1, -1]]) @ np.diag(dscale.ravel()))
+nerr = np.count_nonzero(err)
+bands = (np.floor(np.array([0.16, 0.5, 0.85]) * (svsims - nerr))).astype(int)
+ir_sort = np.sort(ir[:, :, :, ~err.astype('bool').ravel()], 3)
+
+""" Generate Impulse Response Plots """
+# Plot using a function
+ir_plots(ir_sort, bands, scale_adj, s)
+plt.show()
+
+# make ir plots
+
+plt.plot( (ir_sort[0, 1, :, bands[[0, 2]]] * scale_adj[0, 1]).T, c='cadetblue', linewidth=2,
+         linestyle='dashed')
+plt.plot((ir_sort[0, 1, :, bands[[1]]] * scale_adj[0, 1]).T, c='teal', linewidth=3)
+plt.title('Impulse Response of Variable x to y')
+plt.ylabel('Impulse Response')
+plt.xlabel('Time (in quarters)')
+plt.show()
+
+plt.plot( (ir_sort[1, 1, :, bands[[0, 2]]] * scale_adj[0, 1]).T, c='cadetblue', linewidth=2,
+         linestyle='dashed')
+plt.plot((ir_sort[1, 1, :, bands[[1]]] * scale_adj[0, 1]).T, c='teal', linewidth=3)
+plt.title('Impulse Response of variable y to y')
+plt.ylabel('Impulse Response')
+plt.xlabel('Time (in quarters)')
+plt.show()
+
+plt.plot( (ir_sort[2, 1, :, bands[[0, 2]]] * scale_adj[0, 1]).T, c='cadetblue', linewidth=2,
+         linestyle='dashed')
+plt.plot((ir_sort[2, 1, :, bands[[1]]] * scale_adj[0, 1]).T, c='teal', linewidth=3)
+plt.title('Impulse Response of variable z to y')
+plt.ylabel('Impulse Response')
+plt.xlabel('Time (in quarters)')
+plt.show()
+
+plt.plot( (ir_sort[0, 1, :, bands[[0, 2]]] * scale_adj[0, 1]).T, c='cadetblue', linewidth=2,
+         linestyle='dashed')
+plt.plot((ir_sort[1, 1, :, bands[[1]]] * scale_adj[0, 1]).T, c='teal', linewidth=3)
+plt.title('Impulse Response of variable y to y, different confidence bands')
+plt.ylabel('Impulse Response')
+plt.xlabel('Time (in quarters)')
+plt.show()
---
+++ tvpVAR/diagnostics.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tvpVAR.utils.ineff_factor import ineff_factor
+#import tvpVAR.utils.settings as settings
+
+
+#settings.init()
+
+# Load the relevant np.ndarrays from MCMC sampler results file saved in .npz format
+data = np.load('resultsMCMC.npz')
+s_beta = data['s_beta']
+cidx = data['cidx']
+bidx = data['bidx']
+nk = data['nk'].item()
+t = data['t'].item()
+svsims = data['svsims'].item()
+s_om_st = data['s_om_st']
+
+mbeta0 = np.mean(s_beta, axis=2).T
+mbeta = np.zeros(mbeta0.shape)
+mbeta[:, :np.count_nonzero(cidx)] = mbeta0[:, cidx.ravel()]
+mbeta[:, np.count_nonzero(cidx):] = mbeta0[:, bidx.ravel()]
+
+# MCM mixing analysis and beta plots
+ef_om_st, _ = ineff_factor(s_om_st)
+ef_beta, _ = ineff_factor(np.reshape(s_beta, (nk * t, svsims), order='F'))
+
+# Plotting
+flierprops = dict(marker='+', markerfacecolor='teal', markersize=7, linestyle='none', markeredgecolor='teal')
+medianprops = dict(color='lightseagreen')
+plt.boxplot([ef_om_st[:, 0], ef_beta[:, 0]], labels=['Omega Star', 'Beta'], flierprops=flierprops,
+            medianprops=medianprops, showmeans=True, meanline=True)
+plt.xlabel('Estimated Parameters')
+plt.ylabel('Inefficiency Factors')
+plt.title(' Boxplot of Inefficiency Factors')
+plt.show()
+# A boxplot of Omega_star and Bet inefficiency factors
+plt.plot(np.max(mbeta, axis=0) - np.min(mbeta, axis=0), marker='x', c='teal', linestyle='dashed', linewidth=1)
+plt.ylabel('Max Beta - Min Beta')
+plt.xlabel('Parameter Index')
+plt.title('Maximum vs Minimum Average Beta Parameters')
+plt.show()
+
